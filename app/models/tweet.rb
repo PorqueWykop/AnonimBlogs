5c728c121ecdad65496f10ede1ebbcc506857785
PorqueWykop <porquewykop@gmail.com>
2013-02-19 15:45:35 +0100

    Test

---
+++ app/models/tweet.rb
@@ -0,0 +1,9 @@
+class Tweet < ActiveRecord::Base
+  attr_accessible :status
+  validates :status, :presence => true
+
+  def as_json(options = nil)
+  	super(options ||
+  		{only:status})
+  end
+end
